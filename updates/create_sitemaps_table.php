d31f0d9c9f87b3d8bd767567cc8e14bc73e97ae6
agenciazmark <77156328+agenciazmark@users.noreply.github.com>
2022-05-12 13:41:00 -0300

    Update create_sitemaps_table.php

--- updates/create_sitemaps_table.php
+++ updates/create_sitemaps_table.php
@@ -1,7 +1,7 @@
 <?php namespace Zmark\Seo\Updates;
 
 use Schema;
-use October\Rain\Database\Updates\Migration;
+use Winter\Storm\Database\Updates\Migration;
 
 class CreateSitemapsTable extends Migration
 {
